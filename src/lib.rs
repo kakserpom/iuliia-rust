a5fde09c5a834ba3108c4c2c9af4ffe1269ad33f
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2025-04-15 04:01:09 +0700

    A more idiomatic Rust code

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@ extern crate include_dir;
 extern crate regex;
 
 use include_dir::Dir;
-use regex::Regex;
 use lazy_static::lazy_static;
+use regex::Regex;
 
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
+use std::iter::once;
 
 const SCHEMA_DIR: Dir = include_dir!("./iuliia");
 const DUMMY_SYMBOL: &str = "$";
@@ -28,49 +29,34 @@ pub struct Schema {
 impl Schema {
     /// Return Schema object by schema name
     pub fn for_name(s: &str) -> Schema {
-        let schema_file = SCHEMA_DIR.get_file(format!("{}{}", s, ".json"))
+        let schema_file = SCHEMA_DIR
+            .get_file(format!("{}{}", s, ".json"))
             .expect(&format!("There are no schema with name {}", s));
         serde_json::from_str(schema_file.contents_utf8().unwrap()).unwrap()
     }
 
-    pub fn get_pref(&self, s: &str) -> Option<String> {
-        if self.prev_mapping.is_none() {
-            return None;
-        }
-        match self.prev_mapping.as_ref().unwrap().get(&s.replace(DUMMY_SYMBOL.clone(), "").to_lowercase()) {
-            Some(result) => Some(result.clone()),
-            None => None
-        }
+    pub fn get_pref(&self, s: &str) -> Option<&String> {
+        self.prev_mapping
+            .as_ref()
+            .and_then(|x| x.get(&s.replace(DUMMY_SYMBOL, "").to_lowercase()))
     }
 
-    pub fn get_next(&self, s: &str) -> Option<String> {
-        if self.next_mapping.is_none() {
-            return None;
-        }
-        match self.next_mapping.as_ref().unwrap().get(&s.replace(DUMMY_SYMBOL.clone(), "").to_lowercase()) {
-            Some(result) => Some(result.clone()),
-            None => None
-        }
+    pub fn get_next(&self, s: &str) -> Option<&String> {
+        self.next_mapping
+            .as_ref()
+            .and_then(|x| x.get(&s.replace(DUMMY_SYMBOL, "").to_lowercase()))
     }
 
-    pub fn get_letter(&self, s: &str) -> Option<String> {
-        if self.mapping.is_none() {
-            return None;
-        }
-        match self.mapping.as_ref().unwrap().get(&s.to_lowercase()) {
-            Some(result) => Some(result.clone()),
-            None => None
-        }
+    pub fn get_letter(&self, s: &str) -> Option<&String> {
+        self.mapping
+            .as_ref()
+            .and_then(|x| x.get(&s.replace(DUMMY_SYMBOL, "").to_lowercase()))
     }
 
-    pub fn get_ending(&self, s: &str) -> Option<String> {
-        if self.ending_mapping.is_none() {
-            return None;
-        }
-        match self.ending_mapping.as_ref().unwrap().get(&s.to_lowercase()) {
-            Some(result) => Some(result.clone()),
-            None => None
-        }
+    pub fn get_ending(&self, s: &str) -> Option<&String> {
+        self.ending_mapping
+            .as_ref()
+            .and_then(|x| x.get(&s.to_lowercase()))
     }
 }
 
@@ -83,7 +69,6 @@ impl Schema {
 pub fn parse_by_schema_name(s: &str, schema_name: &str) -> String {
     let schema = Schema::for_name(schema_name);
     parse_by_schema(&s, &schema)
-
 }
 
 /// Transliterate a slice of str using `Schema` to `String`
@@ -93,7 +78,7 @@ pub fn parse_by_schema_name(s: &str, schema_name: &str) -> String {
 /// let input = "Юлия, съешь ещё этих мягких французских булок из Йошкар-Олы, да выпей алтайского чаю";
 /// let expected = "Yuliya, syesh yeshchyo etikh myagkikh frantsuzskikh bulok iz Yoshkar-Oly, da vypey altayskogo chayu";
 /// let schema = iuliia_rust::Schema::for_name("wikipedia");
-/// 
+///
 /// let transliterated_word = iuliia_rust::parse_by_schema(&input, &schema);
 ///
 /// assert_eq!(transliterated_word, expected)
@@ -109,58 +94,47 @@ pub fn parse_by_schema(s: &str, schema: &Schema) -> String {
 }
 
 fn parse_word_by_schema(s: &str, schema: &Schema) -> String {
-    let word_by_letters: Vec<String> = s.chars()
-        .map(|char| char.to_string())
-        .collect::<Vec<_>>();
-
+    let word_by_letters: Vec<String> = s.chars().map(|char| char.to_string()).collect::<Vec<_>>();
     //Parse ending
     let ending = parse_ending(&word_by_letters, schema);
-    let mut parsed_end = String::new();
-    let word_without_ending = match ending {
-        Some(matched) => {
-            parsed_end = matched.translate;
-            word_by_letters[..matched.ending_start].to_vec()
-        }
-        None => word_by_letters
+    let (parsed_end, word_without_ending) = if let Some(ending) = ending {
+        (
+            ending.translate,
+            word_by_letters[..ending.ending_start].to_vec(),
+        )
+    } else {
+        (String::new(), word_by_letters)
     };
 
     //Add dummy symbols for window function
-    let mut word_for_parse: Vec<String> = Vec::with_capacity(word_without_ending.len() + 2);
-    let dummy_string: Vec<String> = vec![String::from(DUMMY_SYMBOL.clone())];
-    word_for_parse.extend(dummy_string.clone());
-    word_for_parse.extend(word_without_ending);
-    word_for_parse.extend(dummy_string);
-
     //Parse each letter
-    let parsed_word: String = word_for_parse
+    once(DUMMY_SYMBOL.into())
+        .chain(word_without_ending)
+        .chain(once(DUMMY_SYMBOL.into()))
+        .collect::<Vec<_>>()
         .windows(3)
         .map(|letter_with_neighbors| parse_letter(letter_with_neighbors, schema))
-        .collect();
-
-    //Concat with ending
-    format!("{}{}", parsed_word, parsed_end)
+        .chain(once(parsed_end))
+        .collect::<String>()
 }
 
 fn parse_ending(s: &Vec<String>, schema: &Schema) -> Option<Ending> {
     let length = s.len();
     if length < 3 {
-        return None;
-    }
-
-    match schema.get_ending(&s[length - 1..].concat()) {
-        Some(matched) => return Some(Ending {
-            translate: propagate_case_from_source(matched, &s[length - 1..].concat(), false),
+        None
+    } else if let Some(matched) = schema.get_ending(&s[length - 1..].concat()) {
+        Some(Ending {
+            translate: propagate_case_from_source(&matched, &s[length - 1..].concat(), false),
             ending_start: length - 1,
-        }),
-        None => ()
-    };
-    return match schema.get_ending(&s[length - 2..].concat()) {
-        Some(matched) => return Some(Ending {
-            translate: propagate_case_from_source(matched, &s[length - 2..].concat(), false),
+        })
+    } else if let Some(matched) = schema.get_ending(&s[length - 2..].concat()) {
+        Some(Ending {
+            translate: propagate_case_from_source(&matched, &s[length - 2..].concat(), false),
             ending_start: length - 2,
-        }),
-        None => None
-    };
+        })
+    } else {
+        None
+    }
 }
 
 struct Ending {
@@ -177,23 +151,19 @@ fn parse_letter(letter_with_neighbors: &[String], schema: &Schema) -> String {
     let prefix: String = letter_with_neighbors[..2].concat();
     let postfix: String = letter_with_neighbors[1..].concat();
     let letter: String = letter_with_neighbors[1..2].concat();
-    let mut result = letter.clone();
-    match schema.get_letter(&letter) {
-        Some(matched) => result = matched,
-        None => ()
-    };
-    match schema.get_next(&postfix) {
-        Some(matched) => result = matched,
-        None => ()
-    };
-    match schema.get_pref(&prefix) {
-        Some(matched) => result = matched,
-        None => ()
-    };
+    let result = schema
+        .get_pref(&prefix)
+        .or_else(|| schema.get_next(&postfix))
+        .or_else(|| schema.get_letter(&letter))
+        .unwrap_or(&letter);
     propagate_case_from_source(result, &letter, true)
 }
 
-fn propagate_case_from_source(result: String, source_letter: &str, only_first_symbol: bool) -> String {
+fn propagate_case_from_source(
+    result: &String,
+    source_letter: &str,
+    only_first_symbol: bool,
+) -> String {
     // Determinate case of letter
     let letter_upper = source_letter.chars().any(|letter| letter.is_uppercase());
 
@@ -203,19 +173,19 @@ fn propagate_case_from_source(result: String, source_letter: &str, only_first_sy
 
     if only_first_symbol {
         let mut c = result.chars();
-        match c.next() {
-            None => String::new(),
-            Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+        if let Some(f) = c.next() {
+            f.to_uppercase().collect::<String>() + c.as_str()
+        } else {
+            String::new()
         }
     } else {
         result.to_uppercase()
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::{Schema, parse_by_schema};
+    use crate::{parse_by_schema, Schema};
 
     #[test]
     fn schema_test() {
@@ -231,7 +201,8 @@ mod tests {
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
@@ -247,7 +218,8 @@ mod tests {
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
@@ -263,7 +235,8 @@ mod tests {
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
@@ -279,7 +252,8 @@ mod tests {
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
@@ -295,7 +269,8 @@ mod tests {
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
@@ -306,12 +281,16 @@ mod tests {
     #[test]
     fn test_sentence() {
         //Given
-        let test_words = vec!["Юлия, съешь ещё этих мягких французских булок из Йошкар-Олы, да выпей алтайского чаю", "ВЕЛИКИЙ"];
+        let test_words = vec![
+            "Юлия, съешь ещё этих мягких французских булок из Йошкар-Олы, да выпей алтайского чаю",
+            "ВЕЛИКИЙ",
+        ];
         let expected_words = vec!["Yuliya, syesh yeshchyo etikh myagkikh frantsuzskikh bulok iz Yoshkar-Oly, da vypey altayskogo chayu", "VELIKY"];
         let schema = Schema::for_name("wikipedia");
 
         //When
-        let transliterated_words: Vec<String> = test_words.iter()
+        let transliterated_words: Vec<String> = test_words
+            .iter()
             .map(|word| parse_by_schema(&word, &schema))
             .collect();
 
